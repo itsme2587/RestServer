1d28b9c21b4c5e8e177073d535b9c6f7572a115a
Aman Chopra <amanchopra2587@gmail.com>
2019-07-23 15:53:30 +0530

    My dev commit

--- src/test/java/com/example/restServ/RunCucumberTest.java
+++ src/test/java/com/example/restServ/RunCucumberTest.java
@@ -9,5 +9,4 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"}, features= "src/test/java", snippets= SnippetType.CAMELCASE)
 public class RunCucumberTest {
-
 }
